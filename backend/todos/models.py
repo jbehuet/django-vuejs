3e50beb848f72f520f5a041688fee548f085151b
jbehuet <jbehuet@gmail.com>
2016-04-30 19:57:10 +0200

    Cleanup

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import datetime
 
 class Todo(models.Model):
     description = models.TextField()
+    createdAt = models.DateTimeField(default=datetime.now, blank=True)
